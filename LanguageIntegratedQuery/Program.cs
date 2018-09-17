804e70a8c7c46a7540c1226f1fd4b39acfb7dced
JDiamond <jdiamond@keyedin.com>
2018-09-17 17:24:35 +0100

    initial commit

--- LanguageIntegratedQuery/Program.cs
+++ LanguageIntegratedQuery/Program.cs
@@ -6,10 +6,37 @@ using System.Threading.Tasks;
 
 namespace LanguageIntegratedQuery
 {
+    public delegate int IntDel(int a, int b);
     class Program
     {
+        static Data d = new Data();
         static void Main(string[] args)
         {
+            Console.WriteLine("# of users: {0}", d.users.Count);
+
+            // WRITE CODE IN MAIN THAT INITIALIZES AND INVOKES THE DELEGATE
+            var delA = new IntDel(Add);
+            IntDel delB = Add;
+
+            // INVOKE THE DELEGATE
+            int resultA1 = delA.Invoke(1, 2);
+            int resultA2 = delB.Invoke(2, 3);
+
+            Console.WriteLine("{0} {1}",resultA1, resultA2);
+
+            foreach (var item in d.users)
+            {
+                Console.WriteLine("First name: {0}", item.F_name);
+            }
+
+
+            Console.Write("Press any key to continue. . .");
+            Console.ReadKey();
+        }
+        
+        static int Add(int a, int b)
+        {
+            return a + b;
         }
     }
 }
---
+++ LanguageIntegratedQuery/User.cs
@@ -0,0 +1,20 @@
+﻿namespace LanguageIntegratedQuery
+{
+    internal class User
+    {
+        public User(int id, string f_name, string l_name, string email, char gender)
+        {
+            this.ID = id;
+            this.F_name = f_name;
+            this.L_name = l_name;
+            this.Email = email;
+            this.Gender = gender;
+        }
+        public int ID { get; set; }
+        public string F_name { get; set; }
+        public string L_name { get; set; }
+        public string Email { get; set; }
+        public char Gender { get; set; }
+
+    }
+}
