1cf9c40466220c1b3af8c7efa351cca5e74f8cb9
JDiamond <jdiamond@keyedin.com>
2018-09-18 17:28:49 +0100

    more data

--- LanguageIntegratedQuery/Program.cs
+++ LanguageIntegratedQuery/Program.cs
@@ -1,19 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LanguageIntegratedQuery
 {
     public delegate int IntDel(int a, int b);
-    class Program
-    {
-        static Data d = new Data();
-        static void Main(string[] args)
-        {
-            Console.WriteLine("# of users: {0}", d.users.Count);
 
+    internal class Program
+    {
+        private static Data d = new Data();
+        static List<int> numbers1 = new List<int>() { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+        static List<int> numbers2 = new List<int>() { 15, 14, 11, 13, 19, 18, 16, 17, 12, 10 };
+
+        private static void Main(string[] args)
+        {
             // WRITE CODE IN MAIN THAT INITIALIZES AND INVOKES THE DELEGATE
             var delA = new IntDel(Add);
             IntDel delB = Add;
@@ -22,21 +22,159 @@ namespace LanguageIntegratedQuery
             int resultA1 = delA.Invoke(1, 2);
             int resultA2 = delB.Invoke(2, 3);
 
-            Console.WriteLine("{0} {1}",resultA1, resultA2);
+            // QUERIES!!!
+            // Standard Query
+            IEnumerable<int> highScoresQuery = // Query Variable
+                from number in d.users // Required
+                where number.Fav > 50 // Optional
+                orderby number.Fav descending // Optional
+                select number.Fav; // Must end in Select or Groupby
 
-            foreach (var item in d.users)
-            {
-                Console.WriteLine("First name: {0}", item.F_name);
-            }
+            // Projection - Int to a String
+            IEnumerable<string> highScoresQuery2 =
+                from number in d.users
+                where number.Fav > 80
+                orderby number.Fav descending
+                select $"The score is {number}";
 
+            // Count Expression
+            int highScoreCount =
+                (from number in d.users
+                 where number.Fav > 80
+                 select number.Fav)
+                 .Count();
+            /********* OR ***********/
+            IEnumerable<int> highScoresQuery3 =
+                from number in d.users
+                where number.Fav > 80
+                select number.Fav;
+            int scoreCount = highScoresQuery3.Count();
+
+            // Groupby
+            var queryNumberGroup =
+                (from number in d.users
+                 group number by number.Fav >= 75);
+
+            // Projection - Anonnymous Type
+            var favNumQuery =
+                from user in d.users
+                select new { name = user.F_name + " " + user.L_name, number = user.Fav };
+
+            // averageQuery is an IEnumerable<IGrouping<int, User>>
+            var averageQuery =
+                from user in d.users
+                let tenth = (int)user.Fav / 10
+                group user by tenth into userGrouped
+                orderby userGrouped.Key descending
+                where userGrouped.Key > 5
+                select userGrouped;
+
+            // Filtering using Where clause
+            var rangeFavs =
+                from user in d.users
+                where user.Fav > 50 && user.Fav < 80
+                select new { name = user.F_name + " " + user.L_name, favNum = user.Fav };
+
+            // Orderby Clause
+            IEnumerable<User> userQuery =
+                from user in d.users
+                orderby user.F_name, user.L_name descending
+                select user;
+
+            // Join Clause
+            var favFimlQuery =
+                from user in d.users
+                join film in d.films on user.Fav equals film.FilmID
+                orderby user.F_name, user.L_name
+                select new { user = user.F_name + " " + user.L_name, film.FilmTitle };
+
+            // Let Clause
+            string[] names = { "Svetlana Omelchenko", "Claire O'Donnell", "Sven Mortensen", "Cesar Garcia" };            IEnumerable<string> queryFirstNames =
+                from name in names
+                let firstName = name.Split(' ')[0]
+                select firstName;
+            // SubQueries
+            var queryGroupMax =
+                from student in d.students
+                group student by student.Year into studentGroup
+                select new
+                {
+                    Level = studentGroup.Key,
+                    HighestScore =
+                    (from student2 in studentGroup
+                     select student2.ExamScores.Average())
+                    .Max()
+                };
+
+            // Query Syntax
+            // Query #1
+            IEnumerable<int> numbersList = numbers1.Concat(numbers2);            IEnumerable<int> filteringQuery =
+                from number in numbersList
+                where number < 3 || number > 7
+                select number;
+
+            // Query #2
+            IEnumerable<int> orderingQuery =
+                from num in numbersList
+                where num < 3 || num > 7
+                orderby num ascending
+                select num;
+
+            // Method Syntax
+            // Query #1
+            double average = numbers1.Average();
+            // Query #2
+            IEnumerable<int> concatedValues = numbers1.Concat(numbers2);
+
+            // Query #3
+            IEnumerable<int> largeNumbersQuery = numbers2.Where(c => c > 15);
+
+
+
+            // Mixed query and method syntax            // Query #1
+            int numCount1 =
+                (from num in numbers1
+                 where num < 3 || num > 7
+                 select num).Count();
+
+            // Query #2
+            IEnumerable<int> numbersQuery =
+                from num in numbers1
+                where num < 3 || num > 7
+                select num;
+            int numCount2 = numbersQuery.Count();            IEnumerable<int> queryFactorsOfFour =
+                from num in numbers1.Concat(numbers2)
+                where num % 4 == 0
+                select num;            List<int> factorsofFourList = queryFactorsOfFour.ToList();
+
+
+
+
+
+
+            // OUTPUT!!
+
+            
 
             Console.Write("Press any key to continue. . .");
             Console.ReadKey();
         }
-        
-        static int Add(int a, int b)
+
+        private static int Add(int a, int b)
         {
             return a + b;
+        }
+
+        public static int[] getNumbers(List<User> users)
+        {
+            int[] favnumbers = new int[100];
+            int i = 0;
+            foreach (var item in users)
+            {
+                favnumbers[i] = item.Fav;
+                i++;
+            }
+            return favnumbers;
         }
     }
 }
--- LanguageIntegratedQuery/User.cs
+++ LanguageIntegratedQuery/User.cs
@@ -1,20 +1,46 @@
-﻿namespace LanguageIntegratedQuery
+﻿using System;
+using System.Collections.Generic;
+
+namespace LanguageIntegratedQuery
 {
     internal class User
     {
-        public User(int id, string f_name, string l_name, string email, char gender)
+        static Random r = new Random();
+        public User(int id, string f_name, string l_name, string email, char gender, int fav)
         {
             this.ID = id;
             this.F_name = f_name;
             this.L_name = l_name;
             this.Email = email;
             this.Gender = gender;
+            this.Fav = fav;
+            this.Age = getRandomAge();
+            this.AgeGroup = classify(this.Age);
         }
         public int ID { get; set; }
         public string F_name { get; set; }
         public string L_name { get; set; }
         public string Email { get; set; }
         public char Gender { get; set; }
+        public int Fav { get; set; }
+        public int Age { get; set; }
+        public string AgeGroup { get; set; }
 
+        public static int getRandomAge()
+        {
+            return r.Next(15,56);
+        }
+
+        public string classify(int age)
+        {
+            if (age >= 15 && age < 18)
+                return "Teenager";
+            else if (age >= 18 && age < 30)
+                return "Young Adult";
+            else if (age >= 30 && age < 50)
+                return "Adult";
+            else
+                return "Senior";
+        }
     }
 }
